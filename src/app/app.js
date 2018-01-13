e7c2d67d577cb746724c0e297b4fd50690605f63
PalumboN <nahuel.palumbo@gmail.com>
2018-01-13 03:13:11 -0300

    App client cheta

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,7 @@
-const app = angular.module('bileto.shows', ['ui.router'])
+const app = angular.module('bileto.shows', ['ui.router', 'angularMoment'])
 
 app.controller('ShowsController', ShowsController)
 
 app.config(routes)
+
+moment.locale('es')
--- src/app/controllers.js
+++ src/app/controllers.js
@@ -1,4 +1,3 @@
-
 class ShowsController {
   constructor($http) {
     this.$http = $http
@@ -11,23 +10,33 @@ class ShowsController {
   }
 
   runJobs() {
+    this.runningJob = true
     this.post("jobs/run")
     .then(() => this.loadJobs())
+    .then(() => this.runningJob = false)
   }
 
   searchShow() {
     this.get("shows/" + this.search)
     .then((shows) => {
-      shows.forEach((it) => { it.name = this.search })
-      this.shows = shows
+      shows.forEach((it) => {
+        it.name = this.search
+        it.sections.forEach((it) => it.selected = true)
+      })
       this.show = shows[0]
     })
+  }
+
+  noneSectionSelected() {
+    return !_.some(this.show.sections, "selected")
   }
 
   createNewJob() {
     _.remove(this.show.sections, (it) => !it.selected)
     this.post("jobs", this.show)
+    .then(() => delete this.show)
     .then(() => this.loadJobs())
+    .then(() => $('.collapse').collapse())
   }
 
   get(path) {
