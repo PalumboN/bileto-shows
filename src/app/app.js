489112dcd7ed4d182a85ae0c0c5f46ffbf27a433
PalumboN <nahuel.palumbo@gmail.com>
2018-05-01 04:58:06 -0300

    Hola directivas

--- src/app/app.js
+++ src/app/app.js
@@ -6,6 +6,10 @@ app.controller('ShowsController', ShowsController)
 app.controller('TicketekController', TicketekController)
 app.controller('TicketportalController', TicketportalController)
 
+app.directive('showPerformance', function() { return { templateUrl: 'directives/show-performance.html' }})
+app.directive('ticketekPerformance', function() { return { templateUrl: 'directives/ticketek-performance.html' }})
+app.directive('ticketportalPerformance', function() { return { templateUrl: 'directives/ticketportal-performance.html' }})
+
 app.config(routes)
 
 moment.locale('es')
